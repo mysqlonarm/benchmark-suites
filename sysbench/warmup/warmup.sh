19e472136c24d4d79dab5a4b90e863123d43cccb
Krunal Bauskar <krunal.suresh.bauskar@huawei.com>
2020-03-16 08:28:18 +0000

    - Removed --test options as it is not respected by latest sysbench (old sysbench can work with it)

--- sysbench/warmup/warmup.sh
+++ sysbench/warmup/warmup.sh
@@ -8,4 +8,4 @@ THDS=`nproc`
 sysbench --threads=$THDS --time=$1 --rate=0 --report-interval=5 --db-driver=mysql --rand-type=uniform \
          --mysql-host=$MYSQL_HOST --mysql-port=$MYSQL_PORT --mysql-db=$MYSQL_DB \
          --mysql-user=$MYSQL_USER --mysql-password=$MYSQL_PASSWD \
-         --test=/usr/share/sysbench/oltp_read_only.lua --tables=$TABLES --table-size=$TABLE_SIZE run
+         /usr/share/sysbench/oltp_read_only.lua --tables=$TABLES --table-size=$TABLE_SIZE run
--- sysbench/workload/run.sh
+++ sysbench/workload/run.sh
@@ -7,4 +7,4 @@ TC=$3
 sysbench --threads=$THDS --time=$TIME --rate=0 --report-interval=5 --db-driver=mysql --rand-type=uniform \
          --mysql-host=$MYSQL_HOST --mysql-port=$MYSQL_PORT --mysql-db=$MYSQL_DB \
          --mysql-user=$MYSQL_USER --mysql-password=$MYSQL_PASSWD \
-         --test=$TC --tables=$TABLES --table-size=$TABLE_SIZE run
+         $TC --tables=$TABLES --table-size=$TABLE_SIZE run
